beec3baffce5971999bdb4bb4723c76c70c90352
Shibin Wilson Poovathengal <SHIBINWILSON.POOVATHENGAL@cognizant.com>
2017-10-15 23:43:29 +0530

    modal test

--- app/components/meter-read/submit-meter-read/account-detail/component.js
+++ app/components/meter-read/submit-meter-read/account-detail/component.js
@@ -1,13 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    showModal: false,
     init(){
         this._super(...arguments);
         this.set('email', this.get('accountDetail.email'));
     },  
     actions: {
         onNext() {
-            this.get('transitonTo')(this.get('email'));
+            // this.get('transitonTo')(this.get('email'));
+            this.set('showModal', true);
+        },
+        onCancel() {
+            this.set('showModal', false);
         }
     }
 });
